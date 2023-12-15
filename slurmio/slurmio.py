55069f6889bba8afb1682b33c5ab482c07c19632
Joe Ziminski <55797454+JoeZiminski@users.noreply.github.com>
2023-12-15 18:45:42 +0000

    Update bytes conversion to `1e9` for `GB`
    Just noticed on a 40GB job the returned bytes was 40,000,000, I think this is the correct conversion unless I have made an oversight.

--- slurmio/slurmio.py
+++ slurmio/slurmio.py
@@ -166,6 +166,6 @@ class SlurmJobParameters:
 
 def convert_mem_to_bytes(s):
     if s.endswith("G"):
-        return int(s[:-1]) * 10**6
+        return int(s[:-1]) * 10**9
     else:
         return int(s)
